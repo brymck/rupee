1263fc304dd416857ce25ecab84e7dc6a336fbce
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-10-03 01:13:51 +0900

    Better documentation FindInstance

--- lib/rupee/mixins/find_instance.rb
+++ lib/rupee/mixins/find_instance.rb
@@ -2,11 +2,20 @@ module Rupee
   # For finding instances within a class
   module FindInstance
     # Converts the supplied value to an instance of the specified class (if it
-    # isn't already one)
+    # isn't already one). For example, within the Source object you could find
+    # the Bloomberg object one of two ways:
+    #
+    #   Rupee::Source.find :bloomberg
+    #   Rupee::Source.find Rupee::Source::BLOOMBERG
+    #
+    # This allows the user more flexibility, as specifying the full path to
+    # the <tt>BLOOMBERG</tt> constant is no longer necessary
     def find(x)
       if x.instance_of?(self)
+        # If x is an actual instance of the class, just return it
         x
       else
+        # Otherwise, search for constants after capitalizing x
         self.const_get x.upcase
       end
     end
